eb2335638b65d6632bf64cbbbd021d3b6834b085
Natallia Paliukhovich-Karastsialiova <n.paliukhovich98@gmail.com>
2025-07-04 15:29:56 +0500

    fix: welcome slider

--- js/slider.js
+++ js/slider.js
@@ -3,19 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
     container: ".welcome__slider",
     items: 1,
     slideBy: 1,
-    autoplay: true,
+    autoplay: false,
     mouseDrag: true,
-    autoplayButtonOutput: false,
     controls: false,
     nav: false,
-    center: true,
     touch: true,
-    speed: 1000,
+    speed: 400,
+    loop: true,
+    rewind: false,
     preventScrollOnTouch: 'auto',
   });
-
-  document.querySelector('.slider__btn.prev').addEventListener('click', () => slider.goTo('prev'));
-  document.querySelector('.slider__btn.next').addEventListener('click', () => slider.goTo('next'));
 
   const navButtons = document.querySelectorAll(".slider__dots .dot");
 
@@ -28,5 +25,24 @@ document.addEventListener("DOMContentLoaded", function () {
   slider.events.on("indexChanged", (info) => {
     navButtons.forEach(btn => btn.classList.remove("active"));
     navButtons[info.displayIndex - 1].classList.add("active");
+    document.querySelector(".slider__index-start").textContent = '0' + info.displayIndex;
+  });
+
+  let isAnimating = false;
+
+  slider.events.on("transitionStart", () => {
+    isAnimating = true;
+  });
+
+  slider.events.on("transitionEnd", () => {
+    isAnimating = false;
+  });
+
+  document.querySelector('.slider__btn.prev').addEventListener('click', () => {
+    if (!isAnimating) slider.goTo('prev');
+  });
+
+  document.querySelector('.slider__btn.next').addEventListener('click', () => {
+    if (!isAnimating) slider.goTo('next');
   });
 });
